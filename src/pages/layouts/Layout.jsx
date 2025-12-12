7312108a933061c1442c5f05e2d7e9d010687029
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-12-12 12:04:33 +0630

    button

--- src/pages/layouts/Layout.jsx
+++ src/pages/layouts/Layout.jsx
@@ -20,7 +20,7 @@ export default function Layout() {
                         <span className="text-2xl text-indigo-600 font-bold hidden md:block"> Shelfio </span>
                     </li>
                     <li className='flex items-center gap-2'>
-                        <button className='bg-indigo-500 text-white p-2 md:px-3 md:py-1 rounded-full md:rounded cursor-pointer flex items-center gap-1'>
+                        <button className='bg-indigo-500 text-white p-2 md:px-3 md:py-2 rounded-full md:rounded-2xl cursor-pointer flex items-center gap-1'>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                             </svg>
