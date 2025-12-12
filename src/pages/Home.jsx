118a0d64a1664e54c3df57d607c6f7e2f3c5fcb7
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-12-12 14:09:40 +0630

    animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,15 +6,15 @@ function Home() {
     <>
       <Hero />
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4 my-3">
-        {[1, 2, 3, 4].map(() => (
-          <div className="p-4 border border-gray-300 rounded-xl">
+        {[1, 2, 3, 4].map((key) => (
+          <div className="p-4 border border-gray-300 rounded-xl" key={key}>
             <img src={BookCover} alt="" className="h-45 md:h-70 w-full"/>
             <div className="text-center space-y-2 mt-3">
               <h1>Book Title</h1>
               <p> Description </p>
               <div className="flex flex-wrap">
                 {['romance', 'history', 'knowledge'].map((genre) => (
-                  <span className="mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-blue-400">{genre}</span>
+                  <span className="mx-1 my-1 text-white rounded-full px-2 py-1 text-sm bg-blue-400" key={genre}>{genre}</span>
                 ))}
               </div>
             </div>
--- src/pages/layouts/Layout.jsx
+++ src/pages/layouts/Layout.jsx
@@ -1,13 +1,23 @@
-import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/Navbar.jsx';
+import './styles.css';
+import { Outlet, useLocation } from 'react-router-dom'
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
+import { useRef } from 'react';
 
 export default function Layout() {
+    const location = useLocation();
+    const nodeRef = useRef(null);
+
     return (
         <div>
-            <Navbar/>
-            <div className='max-w-6xl mx-auto p-3'>
-                <Outlet/>
-            </div>
+            <Navbar />
+            <SwitchTransition>
+                <CSSTransition timeout={200} classNames='fade' key={location.pathname} nodeRef={nodeRef}>
+                    <div ref={nodeRef} className='max-w-6xl mx-auto p-3'>
+                        <Outlet/>
+                    </div>
+                </CSSTransition>
+            </SwitchTransition>
         </div>
     )
 }
