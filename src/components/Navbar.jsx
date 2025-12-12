fa54274e211fe53a032d3e4642073e2ebcd260ba
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-12-12 13:05:40 +0630

    router setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Profile from '../assets/profile.png';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
@@ -12,19 +13,19 @@ export default function Navbar() {
 
                 <input type="text" placeholder='Search books' className='outline-none'/>
             </li>
-            <li className='flex items-center gap-2 cursor-pointer md:-ml-32'>
+            <Link to='/' className='flex items-center gap-2 cursor-pointer md:-ml-32'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
                 </svg>
                 <span className="text-2xl text-indigo-600 font-bold hidden md:block"> Shelfio </span>
-            </li>
+            </Link>
             <li className='flex items-center gap-2'>
-                <button className='bg-indigo-500 text-white p-2 md:px-3 md:py-2 rounded-full md:rounded-2xl cursor-pointer flex items-center gap-1'>
+                <Link to="/create" className='bg-indigo-500 text-white p-2 md:px-3 md:py-2 rounded-full md:rounded-2xl cursor-pointer flex items-center gap-1'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
                     <span className='hidden md:block'> Create </span>
-                </button>
+                </Link>
                 <img src={Profile} alt="" className='cursor-pointer w-10 h-10 rounded-full'/>
             </li>
         </ul>
