ad8dc10758a9eb47c1c9af71e03a58f15c1f938c
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-12-15 17:05:52 +0630

    book

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Hero() {
     return (
-    <div className="bg-linear-to-r from-indigo-200 via-red-200 to-yellow-100 h-48 flex justify-center items-center rounded-lg">
+    <div className="bg-linear-to-r from-indigo-200 via-red-200 to-yellow-100 h-48 flex justify-center items-center">
         <div className="space-y-2 text-center">
         <h1 className="text-white text-3xl font-semibold"> Welcome from Shelfio </h1>
         <p className="text-white">Your gateway to knowledge and imagination.</p>
