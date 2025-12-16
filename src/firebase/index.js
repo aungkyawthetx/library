ca16f9e526d15d4aecef8cb1fffba6d1a2ac5310
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-12-16 14:03:17 +0630

    firebase-setup

---
+++ src/firebase/index.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBkoB3-xOrBGPJ4X098gCK-rcg2PtnMX6o",
+  authDomain: "library-web-app-47f85.firebaseapp.com",
+  projectId: "library-web-app-47f85",
+  storageBucket: "library-web-app-47f85.firebasestorage.app",
+  messagingSenderId: "433423907572",
+  appId: "1:433423907572:web:364458d50fd1e6da23fca1",
+  measurementId: "G-4R0BK7CBXK"
+};
+
+const app = initializeApp(firebaseConfig);
