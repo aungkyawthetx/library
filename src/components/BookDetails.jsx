cfcd55773bc2bffd5d552cb954c794c8cff1d742
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-12-19 21:42:29 +0630

    note design

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
 import useTheme from '../hooks/useTheme';
 import { useDocument } from '../hooks/useDocument';
+import Profile from '../assets/profile.png';
 
 export default function BookDetails() {
   let { id } = useParams();
@@ -22,31 +23,49 @@ export default function BookDetails() {
       {error && <p className='text-red-500 fw-semibold'> { error } </p>}
       {loading && <p className='text-indigo-500 fw-semibold'>Loading... </p>}
       {book && (
-      <div className={`max-w-6xl mx-auto p-4 h-screen ${isDark ? 'text-white' : ''}`}>
-        <div className={`grid grid-cols-1 md:grid-cols-3 gap-8 bg-white rounded-2xl shadow-md p-6 border border-gray-200 ${isDark ? 'bg-zinc-900 border-indigo-300 shadow-none' : ''}`}>
-          <div className="flex justify-center">
-            <img
-              src={book.cover}
-              alt="Book Cover"
-              className="w-full rounded-xl shadow-md object-cover"
-            />
-          </div>
+      <> 
+        <div className={`max-w-6xl mx-auto p-4 ${isDark ? 'text-white' : ''}`}>
+          <div className={`grid grid-cols-1 md:grid-cols-3 gap-8 bg-white rounded-2xl shadow-md p-6 border border-gray-200 ${isDark ? 'bg-zinc-900 border-indigo-300 shadow-none' : ''}`}>
+            <div className="flex justify-center">
+              <img
+                src={book.cover}
+                alt="Book Cover"
+                className="w-full rounded-xl shadow-md object-cover"
+              />
+            </div>
 
-          <div className="md:col-span-2 space-y-4">
-            <h1 className={`text-3xl font-bold text-gray-700 uppercase ${isDark ? 'text-indigo-400' : ''}`}> {book.title} </h1>
-              <div className='flex gap-2'>
-                {book.categories.map((genre) => (
-                  <span className={`inline-flex items-center px-3 py-1 text-xs font-medium rounded-full ${classes[genre] || 'bg-indigo-200 text-indigo-700'}`} key={genre}>
-                    {genre}
-                  </span>
-                ))}
-              </div>
-            <p className={`leading-relaxed ${isDark ? 'text-gray-300' : 'text-gray-600'}`}>
-              {book.description}
-            </p>
+            <div className="md:col-span-2 space-y-4">
+              <h1 className={`text-3xl font-bold text-gray-700 uppercase ${isDark ? 'text-indigo-400' : ''}`}> {book.title} </h1>
+                <div className='flex gap-2'>
+                  {book.categories.map((genre) => (
+                    <span className={`inline-flex items-center px-3 py-1 text-xs font-medium rounded-full ${classes[genre] || 'bg-indigo-200 text-indigo-700'}`} key={genre}>
+                      {genre}
+                    </span>
+                  ))}
+                </div>
+              <p className={`leading-relaxed ${isDark ? 'text-gray-300' : 'text-gray-600'}`}>
+                {book.description}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+        <div>
+          <h3 className='text-indigo-500 text-xl font-bold my-3'>My Notes</h3>
+          <textarea className='shadow-sm bg-gray-100 w-full border-2 border-gray-300 rounded-lg p-2 focus:outline-none focus:border-indigo-500 placeholder:italic' placeholder='Enter your notes...' name="" id="" rows="5"></textarea>
+          <div className='border border-gray-300 shadow-md p-3 rounded my-2'>
+            <div className='flex justify-between'>
+              <div className='flex items-center gap-1'>
+                <img src={Profile} alt="Profile Image" className='cursor-pointer w-10 h-10 rounded-full'/>
+                <h3 className='font-bold text-gray-800 mt-1'>Aung Kyaw Thet</h3>
+              </div>
+              <p className='text-sm text-gray-600 italic'>19.12.2025</p>
+            </div>
+            <div className='mt-2'>
+              <p className='text-indigo-800 italic'> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse maxime qui saepe, quisquam expedita asperiores, non voluptates ipsum recusandae et quis harum quasi ad vel voluptatem excepturi fugit sequi nam! </p>
+            </div>
+          </div>
+        </div>
+      </>
       )}
     </>
   )
