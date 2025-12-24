386cc9117a37ae5c5c73a6047e23b24a556ae479
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-12-24 16:04:23 +0630

    delete note

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -2,11 +2,20 @@ import { useParams } from 'react-router-dom';
 import Profile from '../assets/dev.jpeg';
 import { useCollection } from '../hooks/useCollection';
 import useTheme from '../hooks/useTheme';
+import Trash from '../assets/trash.svg';
+import moment from 'moment';
+import useFirestore from '../hooks/useFirestore';
 
 export default function NoteList() {
   let { id } = useParams();
   let { isDark } = useTheme();
-  let { loading, error, data: notes} = useCollection('notes', ['bookUid', '==', id]);
+  let { loading, error, data: notes } = useCollection('notes', ['bookUid', '==', id]);
+  let { deleteDocument } = useFirestore();
+
+  const deleteNote = (e, id) => {
+    e.preventDefault();
+    deleteDocument('notes', id);
+  }
 
   return (
     <>
@@ -19,10 +28,11 @@ export default function NoteList() {
               <img src={Profile} alt="Profile Image" className={`cursor-pointer w-10 h-10 rounded-full ${isDark ? 'border-2 border-indigo-400' : ''}`}/>
               <h3 className={`font-bold mt-1 ${isDark ? 'text-gray-200' : 'text-gray-800'}`}> Your Name </h3>
             </div>
-            <p className={`text-sm italic ${isDark ? 'text-gray-100' : 'text-gray-600'}`}> 12.2.23 </p>
+            <p className={`text-sm italic ${isDark ? 'text-gray-100' : 'text-gray-600'}`}> {moment(n?.date?.seconds * 1000).fromNow()} </p>
           </div>
-          <div className='mt-2'>
+          <div className='flex items-center justify-between mt-2'>
             <p className={`text-sm ${isDark ? 'text-gray-300' : 'text-indigo-800'}`}> {n.note} </p>
+            <img src={Trash} alt="Trash Icon" className='cursor-pointer'onClick={(e) => deleteNote(e, n.id)}/>
           </div>
         </div>
       ))}
